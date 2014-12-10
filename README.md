3824cae834a6a17267e16997b80d453620197a9c
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2014-12-10 14:02:21 +0100

    first commit -workingbase

---
+++ README.md
@@ -0,0 +1,14 @@
+## Foundation 5 SASS Playground
+
+Working base
+
+Preparing for the certification
+
+### using yeoman zf5 generator
+
+    mkdir foundation-sass-playground; cd $_
+    npm remove generator-zf5 -g
+    npm cache clean
+    npm install -g generator-zf5
+    yo zf5
+    grunt
---
+++ app/js/app.js
@@ -0,0 +1,30 @@
+var app = (function(document, $) {
+
+	'use strict';
+	var docElem = document.documentElement,
+
+		_userAgentInit = function() {
+			docElem.setAttribute('data-useragent', navigator.userAgent);
+		},
+		_init = function() {
+			$(document).foundation();
+            // needed to use joyride
+            // doc: http://foundation.zurb.com/docs/components/joyride.html
+            $(document).on("click", "#start-jr", function () {
+                $(document).foundation('joyride', 'start');
+            });
+			_userAgentInit();
+		};
+
+	return {
+		init: _init
+	};
+
+})(document, jQuery);
+
+(function() {
+
+	'use strict';
+	app.init();
+
+})();
